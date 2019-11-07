5a2ac7efdfd011eb47850e7869ccdd8f7b4a17e1
Gonzalo <MendozaGarciaGonzalo>
2019-11-07 09:20:41 -0600

    Added profile user relation

---
+++ users/models.py
@@ -0,0 +1,28 @@
+"""Users model"""
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Profile(models.Model):
+    """ Profile model.
+    Proxy model that extends the base data with other information
+    """
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+
+    website = models.URLField(max_length=200, blank=True)
+    biografy = models.TextField(blank=True)
+    phone_number = models.CharField(max_length=20, blank=True)
+
+    picture = models.ImageField(
+        upload_to='users/picture', 
+        blank=True, 
+        null=True)
+
+    created = models.DateField(auto_now_add=True)
+    modify = models.DateField(auto_now=True)
+
+
+    def ___str__(self):
+        """Return username"""
+        return self.username
