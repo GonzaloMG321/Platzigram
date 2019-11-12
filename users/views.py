bc1c77921ea32d0047c537a72075565a5c5ee9ca
Gonzalo <gonzalo.mendoza.itp@gmail.com>
2019-11-11 22:16:01 -0600

    Logout y signup

--- users/views.py
+++ users/views.py
@@ -1,8 +1,17 @@
 """User views"""
 
 # Django
-from django.contrib.auth import authenticate, login
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+
+# Exceptions
+from django.db.utils import IntegrityError
+# Models
+from users.models import Profile
+
+
 
 def login_view(request):
     """Login view"""
@@ -16,3 +25,30 @@ def login_view(request):
         else:
             return render(request, 'users/login.html', {'error': 'Invalid username and password'})
     return render(request, 'users/login.html')
+
+@login_required
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
+def signup(request):
+    """Sign up view"""
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        password_confirmation = request.POST["password_confirmation"]
+        if password != password_confirmation:
+            return render(request,'users/signup.html', {'error': 'La constraseña no coincide'})
+        try:
+            user = User.objects.create_user(username=username, password=password, )
+        except IntegrityError:
+            return render(request,'users/signup.html', {'error': 'El nombre de usuario ya existe'})
+
+        user.first_name = request.POST['first_name']
+        user.last_name = request.POST['last_name']
+        user.email = request.POST['email']
+        user.save()
+        profile = Profile(user=user)
+        profile.save()
+        return redirect('login')
+    return render(request, 'users/signup.html')
